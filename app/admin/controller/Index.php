d0695e9f51ac0b93e324d75fb05de5acaa3f70a0
layuiYtx <46063996+layuiYtx@users.noreply.github.com>
2018-12-27 03:25:15 +0000

    Add files via upload

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/26
+ * Time: 13:40
+ */
+namespace app\admin\controller;
+class Index
+{
+    public function index(){
+        return "这简直就是受罪啊，WLPL后台见过没有？？？就是这个";
+    }
+}
